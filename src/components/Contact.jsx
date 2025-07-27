74eb02b8a4b2c3b7a45b30650b5f53dcc5b799b6
Jaymi Yam <jaymiyam@gmail.com>
2025-07-27 23:42:54 +0800

    fixed contact section image

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,20 +7,7 @@ const Contact = () => {
       id="contact"
       className="w-screen py-20 md:py-30 px-4 bg-babyBlue-50"
     >
-      <div className="relative bg-[url('images/yeti-3.jpg')] bg-center bg-cover bg-no-repeat rounded-xl px-4 py-20 ">
-        {/* <div className="absolute top-0 -left-16 md:left-0 w-[500px] h-full">
-          <div className="contact-clip-path-1">
-            <img src="images/work-clip-1-poster.png" />
-          </div>
-          <div className="contact-clip-path-2">
-            <img src="images/work-clip-5-poster.png" />
-          </div>
-        </div>
-        <div className="absolute top-0 -right-12 md:right-10 h-full w-96 ">
-          <div className="absolute md:scale-125 swordman-clip-path">
-            <img src="images/yeti-3.jpg" />
-          </div>
-        </div> */}
+      <div className="relative bg-[url('/images/yeti-3.jpg')] bg-center bg-cover bg-no-repeat rounded-xl px-4 py-20 ">
         <div className="relative z-80 flex flex-col items-center justify-center gap-5">
           <p className="uppercase text-xs text-white">get in touch</p>
           <AnimatedTitle>
